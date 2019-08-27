7e0ce601911f03dfb28e5db259aeac045e473b3f
miaoyi <mnmiaoyi@126.com>
2019-08-27 17:52:20 +0800

    写了一个测试Animal类的测试方法

--- src/test/java/TestPOI.java
+++ src/test/java/TestPOI.java
@@ -1,8 +1,13 @@
+import com.baizhi.my.Animal;
 import org.junit.Test;
 
 public class TestPOI {
     @Test
     public void name() {
         System.out.println("miaoyi hello");
+        Animal animal = new Animal();
+        animal.setId("1");
+        animal.setName("可爱的猫咪");
+
     }
 }
