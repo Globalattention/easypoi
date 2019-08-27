1a734f50e2550824d6a14b7113229e73e7cde7cf
liusg <liusg1210@163.com>
2019-08-27 18:29:48 +0800

    liusg  v  3.0

--- src/main/java/com/baizhi/liu/liu.java
+++ src/main/java/com/baizhi/liu/liu.java
@@ -3,5 +3,7 @@ package com.baizhi.liu;
 public class liu {
     public static void main(String[] args) {
         System.out.println("asdas");
+        System.out.println("asdasdddd");
+        System.out.println("v  3.0");
     }
 }
