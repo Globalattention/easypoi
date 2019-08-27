e0d74aebea743548e45c20a1eda58be600827d8c
wz <464529643@qq.com>
2019-08-27 16:06:34 +0800

    666

--- src/main/java/com/baizhi/entrence.java
+++ src/main/java/com/baizhi/entrence.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class entrence {
     public static void main(String[] args) {
         SpringApplication.run(entrence.class, args);
+        System.out.println("ssssssddcsa");
      
     }
 }
